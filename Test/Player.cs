f0962f457081d5843bf32af0780eeb28de240ac4
Shaan <x3zinja12@gmail.com>
2017-01-27 16:19:06 -0500

    Added inverse kinematics for procedural left leg animation

--- Test/Player.cs
+++ Test/Player.cs
@@ -18,13 +18,16 @@ namespace Test
     public class Player : IEntity
     {
         //Variables
-        private Vector2 position, velocity, base_position;
+        private Vector2 position, velocity, base_position, left_foot, right_foot, l_hip, r_hip, l_knee, r_knee;
         private KeyboardState keyboard;
         private bool is_moving;
         private volatile bool shine;
         private float top_speed = 2.0f, friction = 0.1f;
-        
+
+        int pref_rotation = -1;
+
         public static int width = 32, height = 32;
+        private int leg_bone_length = (width / 2);
 
         private Animation test_animation;
 
@@ -42,6 +45,13 @@ namespace Test
             shine = false;
             is_moving = false;
             base_position = new Vector2(position.X + (width / 2), position.Y + height);
+            //Initial left and right foot positions and leg positions
+            left_foot = new Vector2(position.X, position.Y + height * 2 - 5);
+            right_foot = new Vector2(position.X + width, position.Y + height * 2 - 5);
+            l_hip = new Vector2(position.X, position.Y + height);
+            r_hip = new Vector2(position.X + width, position.Y + height);
+            l_knee = new Vector2(position.X + 5, position.Y + height - (height / 2));
+            r_knee = new Vector2(position.X + width + 5, position.Y + height - (height / 2));
             //Initialize animation
             test_animation = new Animation(100.0f, 4-1, 0, 0, width, height);
         }
@@ -66,13 +76,20 @@ namespace Test
         {
             //Handle polling input and velocity
             poll_input();
+            handle_ik();
             //Combine position with velocity, make sure to keep the constraints in mine
             if (velocity.X > top_speed) velocity.X = top_speed;
             if (velocity.Y > top_speed) velocity.Y = top_speed;
 
             position += velocity;
-            //Keep base position constant
+            //Keep base position and feet constant
             base_position = new Vector2(position.X + (width / 2), position.Y + height);
+            //left_foot = new Vector2(position.X, position.Y + height * 2);
+            right_foot = new Vector2(position.X + width, position.Y + height * 2);
+            l_hip = new Vector2(position.X, position.Y + height);
+            r_hip = new Vector2(position.X + width, position.Y + height);
+            //l_knee = new Vector2(position.X, position.Y + height*2 - (height / 2));
+            r_knee = new Vector2(position.X + width, position.Y + height*2 - (height / 2));
             test_animation.update(gameTime);
         }
 
@@ -108,6 +125,20 @@ namespace Test
             {
                 float i = velocity.Y;
                 velocity.Y = i -= friction * i;
+            }
+
+            if (keyboard.IsKeyDown(Keys.K))
+            {
+                left_foot.Y += 1.5f;
+            } else if (keyboard.IsKeyDown(Keys.I))
+            {
+                left_foot.Y -= 1.5f;
+            } else if (keyboard.IsKeyDown(Keys.J))
+            {
+                left_foot.X -= 1.5f;
+            } else if (keyboard.IsKeyDown(Keys.L))
+            {
+                left_foot.X += 1.5f;
             }
 
             //Check if the player is moving
@@ -147,6 +178,39 @@ namespace Test
             }
         }
 
+        //Handle inverse kinematics with a lot of maths lol
+        private void handle_ik()
+        {
+            //Inverse kinematics for the left leg
+            float direction_x = left_foot.X - l_hip.X;
+            float direction_y = left_foot.Y - l_hip.Y;
+            float length = (float)Math.Sqrt(direction_x * direction_x + direction_y * direction_y);
+            direction_x = (direction_x / length);
+            direction_y = (direction_y / length);
+
+            float disc = leg_bone_length * leg_bone_length - length * length / 4;
+            if (disc < 0)
+            {
+                l_knee.X = l_hip.X + direction_x * leg_bone_length;
+                l_knee.Y = l_hip.Y + direction_y * leg_bone_length;
+                left_foot.X = l_hip.X + direction_x * leg_bone_length * 2;
+                left_foot.Y = l_hip.Y + direction_y * leg_bone_length * 2;
+            } else
+            {
+                l_knee.X = l_hip.X + direction_x * length / 2;
+                l_knee.Y = l_hip.Y + direction_y * length / 2;
+                disc = (float)Math.Sqrt(disc);
+                if (pref_rotation < 0)
+                {
+                    disc = -disc;
+                }
+                l_knee.X -= direction_y * disc;
+                l_knee.Y += direction_x * disc;
+            }
+            
+            //TODO: Implement inverse kinematics for the right leg
+        }
+
         public void draw(SpriteBatch spriteBatch)
         {
             //Renderer.FillRectangle(spriteBatch, position, width, height, Color.Purple);
@@ -154,8 +218,16 @@ namespace Test
             //Renderer.FillRectangle(spriteBatch, point_orbit, 5, 5, Color.Green);
             //Renderer.FillRectangle(spriteBatch, origin, 5, 5, Color.Blue);
             //Draw hitbox
-            Renderer.FillRectangle(spriteBatch, position, width, width, Color.Purple);
             spriteBatch.Draw(Constant.spritesheet, position, test_animation.source_rect, Color.White);
+            Renderer.FillRectangle(spriteBatch, left_foot, 5, 5, Color.Cyan);
+            Renderer.FillRectangle(spriteBatch, right_foot, 5, 5, Color.Cyan);
+            Renderer.FillRectangle(spriteBatch, l_hip, 5, 5, Color.Cyan);
+            Renderer.FillRectangle(spriteBatch, r_hip, 5, 5, Color.Cyan);
+            Renderer.FillRectangle(spriteBatch, l_knee, 5, 5, Color.Cyan);
+            Renderer.FillRectangle(spriteBatch, r_knee, 5, 5, Color.Cyan);
+            //Draw legs?
+            Renderer.DrawALine(spriteBatch, Constant.pixel, 1, Color.LightCyan, l_hip, l_knee);
+            Renderer.DrawALine(spriteBatch, Constant.pixel, 1, Color.LightCyan, l_knee, left_foot);
         }
     }
 }
